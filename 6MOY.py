a446dd1dd6c1b056e0c17e49237e60688f5ea0eb
Schemme <157423567+Schemme@users.noreply.github.com>
2024-04-27 19:33:42 -0400

    Update 6MOY.py

--- 6MOY.py
+++ 6MOY.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np 
-
+ 
 st.title('6th man of the year votes')
 df=pd.read_csv('Schemme/FINAL-PROJECT/6MOY.csv')
 
@@ -10,4 +10,15 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(df)
 
-player=st.radio('Select the player')
+st.sidebar.title("Choose votes to Display")
+    first_place = st.sidebar.checkbox("1st Place Votes", value=True)
+    second_place = st.sidebar.checkbox("2nd Place Votes", value=True)
+    third_place = st.sidebar.checkbox("3rd Place Votes", value=True)
+
+
+    fig, ax = plt.subplots()
+    data_to_plot = data[columns_to_display].sum()
+    data_to_plot.plot(kind='bar', ax=ax)
+    ax.set_ylabel('Votes')
+    ax.set_title('Votes Distribution')
+    return fig
