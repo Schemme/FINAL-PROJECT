aba3f36e295342b391d7f627621a3eebdfefd632
Schemme <157423567+Schemme@users.noreply.github.com>
2024-04-28 23:46:21 -0400

    Setting up home page

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from multiapp import MultiApp
-from apps import MVP, 6MOY, per-stats
-app=MultiApp()
-
-st.markdown("""
-#NBA DATA VISUALIZATION DASHBOARD
-##This multi page app is designed to sift through NBA data and as a reflection of the creators thoughts on the data.   
-""")
