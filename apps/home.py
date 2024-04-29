d27040e095d33752a89a6867d420dc3f6cefb5a5
Schemme <157423567+Schemme@users.noreply.github.com>
2024-04-28 23:40:50 -0400

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,27 +1,5 @@
 import streamlit as st
-import 6moy
-import mvp
-import per_stats
+import streamlit as st
 
-def main():
-    st.title('NBA Data Viz Dashboard')
-    st.write("This is our final project")
-    
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('') # Adding some space for better UI
-    
-    st.header('Choose an App')
-    app_choice = st.selectbox('Select an app to run', ['6moy', 'mvp', 'per-stats'])
-    
-    if app_choice == '6moy':
-        6moy.main()
-    elif app_choice == 'mvp':
-        mvp.main()
-    elif app_choice == 'per-stats':
-        per_stats.main()
-
-if __name__ == "__main__":
-    main()
+st.page_link("MVP.py", label="MVP")
+st.page_link("per-stats.py", label="Player Efficiency Rating")
